6b99fddcebc183b1154f54fefcbb2eb8fb27df38
Moloko Sehata <moloko4@live.com>
2023-10-10 09:55:31 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] luckyNumbers = { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Print from a String " + luckyNumbers[2]);
+
+            string[] friends = new string[5];
+            friends[0] = "Moloko";
+            friends[1] = "Lehlohonolo";
+            Console.WriteLine(friends[1]);
+
+
+        }
+    }
+}
